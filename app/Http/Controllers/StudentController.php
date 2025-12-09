cc25d9459d105c953f341eba49d8651202cfc1f0
jerico-lopez <jericolopez2003@gmail.com>
2025-12-09 17:27:10 +0800

    photo edit and delete

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,20 +5,26 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StudentController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $students = Student::all();
+
         return view('students.index', compact('students'));
     }
 
-    public function create(){
+    public function create()
+    {
         $courses = Course::all();
+
         return view('students.create', compact('courses'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'first_name' => 'required|string|max:30',
             'middle_name' => 'nullable|string|max:30',
@@ -28,11 +34,11 @@ class StudentController extends Controller
             'email' => 'required|email|unique:students,email',
             'address' => 'required|string|max:255',
             'course_id' => 'required|exists:courses,id',
-            'photo' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:2048'
+            'photo' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:2048',
         ]);
 
-        if($request->hasFile('photo')){
-            $filename = time() . '.' . $request->photo->extension();
+        if ($request->hasFile('photo')) {
+            $filename = time().'.'.$request->photo->extension();
             $request->photo->storeAs('students', $filename, 'public');
             $validated['photo'] = $filename;
         }
@@ -42,17 +48,22 @@ class StudentController extends Controller
         return redirect('/students')->with('success', 'Student added successfuly');
     }
 
-    public function show(Student $student){
+    public function show(Student $student)
+    {
         $courses = Course::all();
+
         return view('students.show', compact('student', 'courses'));
     }
 
-    public function edit(Student $student){
+    public function edit(Student $student)
+    {
         $courses = Course::all();
+
         return view('students.edit', compact('student', 'courses'));
     }
 
-    public function update(Request $request, Student $student){
+    public function update(Request $request, Student $student)
+    {
         $validated = $request->validate([
             'first_name' => 'required|string|max:30',
             'middle_name' => 'nullable|string|max:30',
@@ -61,19 +72,37 @@ class StudentController extends Controller
             'age' => 'required|numeric|max:30',
             'email' => 'required|email',
             'address' => 'required|string|max:255',
-            'course' => 'required|string|max:20'
+            'course_id' => 'required|exists:courses,id',
+            'photo' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:2048',
         ]);
+
+        if ($request->hasFile('photo')) {
+            // delete old image
+            if ($student->photo && Storage::disk('public')->exists('students/'.$student->photo)) {
+                Storage::disk('public')->delete('students/'.$student->photo);
+            }
+
+            // store new
+            $filename = time().'.'.$request->photo->extension();
+            $request->photo->storeAs('students', $filename, 'public');
+            $validated['photo'] = $filename;
+        }
 
         $student->update($validated);
 
         return redirect('/students')->with('success', 'Student updated successfuly');
     }
 
-    public function delete(Student $student){
+    public function delete(Student $student)
+    {
         return view('students.delete', compact('student'));
     }
 
-    public function destroy(Student $student){
+    public function destroy(Student $student)
+    {
+        if ($student->photo && Storage::disk('public')->exists('students/'.$student->photo)) {
+            Storage::disk('public')->delete('students/'.$student->photo);
+        }
         $student->delete();
 
         return redirect('/students')->with('success', 'Student deleted successfuly');
--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,68 +1,81 @@
 @extends('layout.main')
 @section('content')
-    <form action="/students/update/{{ $student->id }}" method="POST">
-        @method('PUT')
-        @csrf
-        <div class="mb-3">
-            <label for="first_name" class="form-label">Firstname</label>
-            <input type="text" class="form-control" id="first_name" name="first_name" value="{{ $student->first_name }}">
-            @error('first_name')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="middle_name" class="form-label">Middlename</label>
-            <input type="text" class="form-control" id="middle_name" name="middle_name" value="{{ $student->middle_name }}">
-            @error('middle_name')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="last_name" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="last_name" name="last_name" value="{{ $student->last_name }}">
-            @error('last_name')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="age" class="form-label">Age</label>
-            <input type="text" class="form-control" id="age" name="age" value="{{ $student->age }}">
-            @error('age')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="contact_number" class="form-label">Contact Number</label>
-            <input type="text" class="form-control" id="contact_number" name="contact_number" value="{{ $student->contact_number }}">
-            @error('contact_number')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="text" class="form-control" id="email" name="email" value="{{ $student->email }}">
-            @error('email')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="course" class="form-label">Course</label>
-            <select class="form-select" name="course">
-                @foreach ($courses as $course)
-                    <option value="{{ $course }}" {{ $course == $student->course ? 'selected' : '' }}>{{$course->name}}</option>
-                @endforeach
-            </select>
-            @error('course')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="address" class="form-label">Address</label>
-            <input type="text" class="form-control" id="address" name="address" value="{{ $student->address }}">
-            @error('address')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <form action="/students/update/{{ $student->id }}" method="POST" enctype="multipart/form-data">
+            @method('PUT')
+            @csrf
+            <div class="mb-3">
+                <label for="first_name" class="form-label">Firstname</label>
+                <input type="text" class="form-control" id="first_name" name="first_name" value="{{ $student->first_name }}">
+                @error('first_name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="middle_name" class="form-label">Middlename</label>
+                <input type="text" class="form-control" id="middle_name" name="middle_name"
+                    value="{{ $student->middle_name }}">
+                @error('middle_name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="last_name" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="last_name" name="last_name"
+                    value="{{ $student->last_name }}">
+                @error('last_name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="age" class="form-label">Age</label>
+                <input type="text" class="form-control" id="age" name="age" value="{{ $student->age }}">
+                @error('age')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="contact_number" class="form-label">Contact Number</label>
+                <input type="text" class="form-control" id="contact_number" name="contact_number"
+                    value="{{ $student->contact_number }}">
+                @error('contact_number')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" id="email" name="email" value="{{ $student->email }}">
+                @error('email')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="course_id" class="form-label">Course</label>
+                <select class="form-select" name="course_id">
+                    @foreach ($courses as $course)
+                        <option value="{{ $course->id }}" {{ $course == $student->course ? 'selected' : '' }}>
+                            {{ $course->name }}</option>
+                    @endforeach
+                </select>
+                @error('course_id')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Address</label>
+                <input type="text" class="form-control" id="address" name="address" value="{{ $student->address }}">
+                @error('address')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="photo" class="form-label">Photo</label>
+                <input type="file" class="form-control" id="photo" name="photo" value="{{ old('photo') }}">
+                @error('photo')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
